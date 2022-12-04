0d4fdbd785c986ed566ddc45c470426fadc04625
alec sharp <alecsharpie@gmail.com>
2022-12-05 03:18:23 +1100

    start trying to include data from json on the page

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 var request = new XMLHttpRequest();
 
 request.open('GET', 'localhost:8000/projects.json', true);
+console.log(request);
 request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
         // Success!
         var data = JSON.parse(request.responseText);
-        console.log(data)
+        console.log(data);
         for (var key in data) {
-            if (data.hasOwnProperty(key)) {
-                var value = data[key];
-                var container = document.getElementById('container');
-                container.innerHTML += '<p>' + key + ': ' + value + '</p>';
-            }
+            // if (data.hasOwnProperty(key)) {
+            //     var value = data[key];
+            //     var container = document.getElementById('container');
+            //     container.innerHTML += '<p>' + key + ': ' + value + '</p>';
+            // }
+
+            // Create a new div element
+            var div = document.createElement('div');
+
+            // Set the inner HTML of the div to some content
+            div.innerHTML = '<p>This is my new div element with content!</p>';
+
+            // Get a reference to the element with the id "project-container"
+            var container = document.getElementById('project-container');
+
+            // Append the new div to the container element
+            container.appendChild(div);
 
         }
 
