80b50292aa84bad4167950705fa408ef93e44d1e
alec sharp <alecsharpie@gmail.com>
2022-12-15 15:57:00 +1100

    restructure html/js/json files

--- js/educational-resources.js
+++ js/educational-resources.js
@@ -2,17 +2,15 @@ import { writeProjectRow } from './project-writer.js';
 
 
 // Retrieve the contents of the JSON file
-fetch('projects.json')
-    .then(response => response.json()) // Parse the JSON data
+fetch('data/projects.json')
+    .then(response => response.json())
     .then(data => {
 
             // Get a reference to the element with the id "project-container"
             var container = document.getElementById('project-container');
 
-
             // Loop through the keys in the JSON object
-            data['projects'].forEach((project, index) => {
-
+            data['educational'].forEach((project, index) => {
 
                 if (index % 2 == 0) {
                     var side = "left"
@@ -29,7 +27,6 @@ fetch('projects.json')
                 flex_item.className = "flex-item";
 
                 flex_item.innerHTML = `<hr class="projects-split"/>`;
-
 
                 container.appendChild(flex_item);
 
--- js/index.js
+++ js/index.js
@@ -2,15 +2,17 @@ import { writeProjectRow } from './project-writer.js';
 
 
 // Retrieve the contents of the JSON file
-fetch('projects.json')
-    .then(response => response.json())
+fetch('data/projects.json')
+    .then(response => response.json()) // Parse the JSON data
     .then(data => {
 
             // Get a reference to the element with the id "project-container"
             var container = document.getElementById('project-container');
 
+
             // Loop through the keys in the JSON object
-            data['educational'].forEach((project, index) => {
+            data['projects'].forEach((project, index) => {
+
 
                 if (index % 2 == 0) {
                     var side = "left"
@@ -27,6 +29,7 @@ fetch('projects.json')
                 flex_item.className = "flex-item";
 
                 flex_item.innerHTML = `<hr class="projects-split"/>`;
+
 
                 container.appendChild(flex_item);
 
